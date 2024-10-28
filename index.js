49e6530c2f782ba5e90b8d0bcff5acde314983a6
paedDev <jannoelpaed27@gmail.com>
2024-10-29 02:06:20 +0800

    Added responsive to the navbar

---
+++ index.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".navbar-menu");
+const menuIcon = document.querySelector(".menu-icon");
+
+menuIcon.addEventListener("click", ()=>{
+
+    menu.classList.toggle("show")
+    
+    })
